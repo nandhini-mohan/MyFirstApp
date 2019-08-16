b4dd7847da07260d7015055c98c00cebf0afed63
nandhini-mohan <54166293+nandhini-mohan@users.noreply.github.com>
2019-08-16 14:25:35 +0530

    Second Commit
    Second Commit

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-function App() {
+//this is mine
+function Nandhini() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="eyebrow">
+      <h1>Welcome To our Daily Cookbook</h1>
+      <h3>Please enter your recipes</h3>
+      <input type="text" />
+      <input type="button" value="Add" />
     </div>
   );
 }
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Nandhini />, rootElement);
